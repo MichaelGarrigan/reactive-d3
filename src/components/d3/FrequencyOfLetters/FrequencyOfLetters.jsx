a55c79f3ff7c847a474d0d283703b460cfc14d54
M-garrigan <mgarrigan99@gmail.com>
2018-10-06 02:19:25 -0400

    Redo of the Frequency Example.
    Got the bars to render and pulled the csv in to the file in a string.
    It still needs axis and a responsive wrapper.

--- src/components/d3/FrequencyOfLetters/FrequencyOfLetters.jsx
+++ src/components/d3/FrequencyOfLetters/FrequencyOfLetters.jsx
@@ -1,84 +1,60 @@
 
 import React from 'react';
+import {max} from 'd3-array';
 import {axisBottom, axisLeft} from 'd3-axis';
-import {scaleOrdinal, scaleLinear, scaleBand} from 'd3-scale';
-import freqData from '../../../../dist/data/FrequencyOfLetters.csv';
-//console.log('freq:', freqData);
+import {csvParse} from 'd3-dsv';
+import {scaleLinear, scaleBand} from 'd3-scale';
+import {select} from 'd3-selection';
+
 
 class FrequencyOfLetters extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     data: null
-  //   };
-  // }
-  state = {
-    data: null
-  }
-
-  // componentDidMount() {
-  //   console.log('out:', this.state);
-  //   if (this.state.data === null) {
-  //     console.log('inDidMount');
-  //     // convert csv data to an array of objects
-  //     let convertCSV = [];
-  //     for (let i = 1; i < freqData.length; i += 1) {
-  //       convertCSV.push({'id': freqData[i][0], 'value': freqData[i][1]});
-  //     }
-  //     console.log('freq conv.:', convertCSV);
-  //     this.setState({data: convertCSV});
-  //   }
- //}
+  
   render() {
-    //console.log('out:', this.state);
-    let convertCSV = [];
-    //if (this.state.data === null) {
-      //console.log('inDidMount');
-      // convert csv data to an array of objects
-      
-    for (let i = 1; i < freqData.length; i += 1) {
-      convertCSV.push({'id': freqData[i][0], 'value': freqData[i][1]});
-    }
-    //console.log('freq conv.:', convertCSV);
-    //this.setState({data: convertCSV});
-    //}
-    const margin = {top:20, right:30, bottom:30, left:40};
-    const height = 960; 
-    const innerHeight = height - margin.left - margin.right;
-    const width = 500;
-    const innerWidth = width - margin.top - margin.bottom;
+    const csvFrequency = 
+      `letter,frequency\nA,.08167\nB,.01492\nC,.02780\nD,.04253\nE,.12702\nF,.02288\nG,.02022\nH,.06094\nI,.06973\nJ,.00153\nK,.00747\nL,.04025\nM,.02517\nN,.06749\nO,.07507\nP,.01929\nQ,.00098\nR,.05987\nS,.06333\nT,.09056\nU,.02758\nV,.01037\nW,.02465\nX,.00150\nY,.01971\nZ,.00074\n`;
 
-    const x = scaleBand().rangeRound([0, innerWidth]);
-    const y = scaleLinear().range([innerHeight, 0]);
+    let csvParsed = csvParse(
+      csvFrequency, 
+      d => ({letter: d.letter, frequency: +d.frequency})
+    );
+    console.log(csvParsed);
 
-    const xAxis = axisBottom().scale(x);
-    const yAxis = axisLeft().scale(y);
+    const margin = {top: 20, right: 30, bottom: 30, left: 40};
+    const width = 960 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    const x = scaleBand().rangeRound([0, width]);
+    const y = scaleLinear().range([height, 0]);
+
+
     
-    
+    x.domain(csvParsed.map(function(d) { return d.letter; }));
+    y.domain([0, max(csvParsed, function(d) { return d.frequency; })]);
 
+    select('.x').call(axisBottom().scale(x));
+    select('.y').call(axisLeft().scale(y));
+    //console.log('sel',select('#x'));
+    
     return (
       <div>
-        <svg className="svg-frequency-letters" height={height} width={width}>
-          <g transform={`translate(${margin.left},${margin.top}`}>
-            <g 
-              className="x axis"
-              transform={`translate(0,${innerHeight}`}
-            >{xAxis()}
-            </g>
-            <g 
-              className="y axis"
-            >{yAxis()}
-            </g>
+        <svg className="svg-frequency-letters" width={width + margin.left + margin.right} height={height + margin.top + margin.bottom}>
+          <g 
+            style={{transform:`translate(${margin.left},${margin.top})`}}
+          >
+            <g id='xAxis' className='x axis' style={{transform:`translate(0,${height})`}} />
+              
+            <g className='y axis' />
+              
             {
-             
-              convertCSV.map( (item) => {
+              csvParsed.map( datum => {
                 return (
-                  <rect
-                    key={item.name + item.value}
-                    x={x(item.name)}
-                    y={y(item.value)}
-                    height={innerHeight - y(item.value)}
-                    width={x.rangeRound()}
+                  <rect 
+                    key={datum.letter}
+                    className='bar'
+                    x={x(datum.letter)}
+                    y={y(datum.frequency)}
+                    width={30}
+                    height={height - y(datum.frequency)}
                   />
                 );
               })
