81aac9e257b4c59b73dad0f219a97aa2e8740190
M-garrigan <mgarrigan99@gmail.com>
2018-12-07 19:41:31 -0500

    Work on the Scales Editor.
    Built out the main functionality for the scaleLinear function.
    The last input needs an onChange handler to erase the initial value placed into it.
    Also the two domain/range methods are almost identical and can be combined with the
    addition of an additional argument.

--- src/components/editors/ScaleEditor/ScaleEditor.jsx
+++ src/components/editors/ScaleEditor/ScaleEditor.jsx
@@ -1,49 +1,196 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import { scaleLinear } from 'd3-scale';
+
+import './ScaleEditor.css';
 
 export default class ScaleEditor extends Component {
-  render() {
-    const linear1 = d3.scaleLinear()
-      .domain([10, 1000])
-      .range([-10, -1000]).clamp(true);
+  state = {
+    linear: {
+      clamp: false,
+      domain: [0,1],
+      range: [0,1],
+      invert: false,
+      arguments: ['', '', ''],
+      results: ['', '', '']
+    }
+  }
 
-    console.log(linear1(10));
-    console.log(linear1(1400));
+  handleClampingLinear = event => {
+    event.preventDefault();
+    this.setState(prevState => ({
+      linear: {
+        clamp: !prevState.linear.clamp
+      }
+    }));
+  }
 
-    const linear2 = d3.scaleLinear()
-      .domain([1,2,3])
-      .range(['A','B','C']);
+  handleDomainEntry = (event, scaleName) => {
+    event.preventDefault();
 
-    console.log(linear2(1));
-    console.log(linear2(3));
-
-    var linear3 = d3.scaleLinear()
-      .domain([-100, 0, +100])
-      .range(["red", "white", "green"]);
-
-    console.log(linear3(-100));
-    console.log(linear3(0));
-    console.log(linear3(-50));
-    console.log(linear3(150));
-
-    const linear4 = d3.scaleLinear()
-      .domain(['A','B','C'])
-      .range([1,2,3]);
-
-    console.log(linear4('A'));
-    console.log(linear4('C'));
-    console.log(linear4.invert(1));
-    console.log(linear4.invert(3));
-
-    const linear5 = d3.scaleLinear();
-
-    console.log(linear5('red'));
-    console.log(linear5(-8))
-
+    let entryAsString = event.target.value;
+    let splitStringToArray = entryAsString.split(',');
     
+    if (splitStringToArray.length === 0) {
+      this.setState(prevState => ({
+        [scaleName]: { 
+          ...prevState[scaleName],
+          domain: [0,1] 
+        }
+      }));
+    } else {
+      let container = [];
+      splitStringToArray.forEach( item => {
+        
+        if (item !== '' && Object.prototype.toString.call(+item).slice(8, -1) === 'Number') {
+          container.push(+item);
+        } else {
+          container.push(item);
+        }
+      });
+      
+      this.setState(prevState => ({
+        [scaleName]: { 
+          ...prevState[scaleName],
+          domain: container 
+        }
+      }));
+    }
+  }
+
+  handleRangeEntry = (event, scaleName) => {
+    event.preventDefault();
+
+    let entryAsString = event.target.value;
+    let splitStringToArray = entryAsString.split(',');
+    
+    if (splitStringToArray.length === 0) {
+      this.setState(prevState => ({
+        [scaleName]: { 
+          ...prevState[scaleName],
+          range: [0,1] 
+        }
+      }));
+    } else {
+      let container = [];
+      splitStringToArray.forEach( item => {
+        
+        if (item !== '' && Object.prototype.toString.call(+item).slice(8, -1) === 'Number') {
+          container.push(+item);
+        } else {
+          container.push(item);
+        }
+      });
+      
+      this.setState(prevState => ({
+        [scaleName]: { 
+          ...prevState[scaleName],
+          range: container 
+        }
+      }));
+    }
+  }
+
+  executeLinearScale = () => {
+    let scale = scaleLinear().domain(this.state.linear.domain).range(this.state.linear.range);
+    let scaleResult = scale(this.state.linear.arguments[0]);
+    console.log('scale: ', scaleResult);
+
+    this.setState( prevState => ({
+      linear: {
+        ...prevState.linear,
+        results: [scaleResult]
+      }
+    }));
+  }
+
+  handleArgumentsLinear = event => {
+    event.preventDefault();
+    let value = event.target.value;
+    if (value !== '') {
+      this.setState(prevState => ({
+        linear: {
+          ...prevState.linear,
+          arguments: [+value]
+        }
+      }), () => {console.log('here');this.executeLinearScale()}
+      );
+    }
+  }
+  
+
+  render() {
+    console.log('range: ', this.state.linear.range)
     return (
-      <div>
-        d3-scale Editor
+      <div className="scale-editor-wrapper">
+      <h1 className="editor-title">Interact with and explore D3 Scales</h1>
+        <div className="scale-editor-intro">
+          <p>D3 provides 7 categories of scales, some of the categories have multiple scale types.</p>
+        </div>
+
+        <div className="scale-category">
+          <h2>Continuous</h2>
+
+          <div className="scale-continuous-type">
+            <h3>Linear</h3>
+            <span>Enable Clamping</span>
+            <button 
+              type="button" 
+              onClick={e => this.handleClampingLinear(e)}
+            >
+              {this.state.linear.clamp + ''}
+            </button>
+          <p className="scale-continuous-scale-function">
+            const linear = scaleLinear()
+          </p>
+          <div className="scale-continuous-domain">
+            <span>.domain([</span>
+            <input 
+              type="text" name="domain" 
+              id="scaleLinearDomain" 
+              value={this.state.linear.domain}
+              onChange={ e => this.handleDomainEntry(e, 'linear')}
+            />
+            <span>])</span>
+          </div>
+          <div className="scale-continuous-range">
+            <span>.range([</span>
+            <input type="text" name="range" id="scaleLinearRange"
+              value={this.state.linear.range}
+              onChange={ e => this.handleRangeEntry(e, 'linear')}
+            />
+            <span>])</span>
+          </div>
+          {
+            this.state.linear.clamp 
+              ? (
+                  <p className="scale-continuous-scale-function">
+                    .clamp(true)
+                  </p>
+                )
+              : ''
+          }
+
+          <div className="scale-continuous-results-wrapper">
+            
+            <div className="scale-continuous-results-each">
+              <span>linear(</span>
+              <input 
+                type="text" name="range" 
+                id="scaleLinearRange"
+                value={this.state.linear.arguments[0]}
+                onChange={ e => this.handleArgumentsLinear(e)}
+              />
+              <span>)</span>
+              <span>=></span>
+              <span>{this.state.linear.results[0]}</span>
+            </div>
+
+
+          </div>
+          </div>
+        </div>
+      
       </div>
     )
   }
