fc0083c72f955bbe28064852d1ceabbef37d319b
M-garrigan <mgarrigan99@gmail.com>
2019-07-13 20:12:02 -0400

    Convert from class component into functional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Router, Route, Link, Switch } from 'react-router-dom';
 
 import AnalogClock from './d3/AnalogClock/AnalogClock.jsx';
@@ -14,6 +14,7 @@ import Force1 from './d3/Force/force1/Force1.jsx';
 import Force2 from './d3/Force/force2/Force2.jsx';
 import Force3 from './d3/Force/force3/Force3.jsx';
 import FrequencyOfLetters from './d3/FrequencyOfLetters/FrequencyOfLetters.jsx';
+import GagueCircle1 from './d3/gague/circle1/GagueCircle1.jsx';
 import Home from './home/Home.jsx';
 import LinkedListEditor from './editors/LinkedListEditor/LinkedListEditor.jsx';
 import LineChart from './d3/line/LineChart.jsx';
@@ -30,45 +31,42 @@ import TopoLower48 from './d3/maps/lower48/TopoLower48.jsx';
 import Tux_SVG from './d3/SVG/tux/Tux_SVG.jsx';
 import USMap1 from './d3/maps/usMap1/USMap1.jsx';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="global-container">
-        <NavBar />
-  
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/AnalogClock' component={AnalogClock} />
-          <Route exact path='/AnimatedCircles' component={AnimatedCircle} />
-          <Route exact path='/Area1' component={Area1} />
-          <Route exact path='/Blog' component={Blog} />
-          <Route exact path='/CirclePack1' component={CirclePack1} />
-          <Route exact path='/DigitalClock' component={DigitalClock} />
-          <Route exact path='/EditorShell' component={EditorShell} />
-          <Route exact path='/Examples' component={Examples} />
-          <Route exact path='/Force1' component={Force1} />
-          <Route exact path='/Force2' component={Force2} />
-          <Route exact path='/Force3' component={Force3} />
-          <Route exact path='/FrequencyOfLetters' component={FrequencyOfLetters} />
-          <Route exact path='/LinkedListEditor' component={LinkedListEditor} />
-          <Route exact path='/LineChart' component={LineChart} />
-          <Route exact path='/LineEditor' component={LineEditor} />
-          <Route exact path='/Modules' component={Modules} />
-          <Route exact path='/Pie1' component={Pie1} />
-          <Route exact path='/ScaleEditor' component={ScaleEditor} />
-          <Route exact path='/ShapeEditor' component={ShapeEditor} />
-          <Route exact path='/Playground' component={Playground} />
-          <Route exact path='/StackChart' component={StackChart} />
-          <Route exact path='/Top5States' component={Top5States} />
-          <Route exact path='/TopoLower48' component={TopoLower48} />
-          <Route exact path='/Tux_SVG' component={Tux_SVG} />
-          <Route exact path='/USMap1' component={USMap1} />
+const App = () => (
+  <div className="global-container">
+    <NavBar />
 
-        </Switch>
-        
-      </div>
-    )
-  }
-};
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/AnalogClock' component={AnalogClock} />
+      <Route exact path='/AnimatedCircles' component={AnimatedCircle} />
+      <Route exact path='/Area1' component={Area1} />
+      <Route exact path='/Blog' component={Blog} />
+      <Route exact path='/CirclePack1' component={CirclePack1} />
+      <Route exact path='/DigitalClock' component={DigitalClock} />
+      <Route exact path='/EditorShell' component={EditorShell} />
+      <Route exact path='/Examples' component={Examples} />
+      <Route exact path='/Force1' component={Force1} />
+      <Route exact path='/Force2' component={Force2} />
+      <Route exact path='/Force3' component={Force3} />
+      <Route exact path='/FrequencyOfLetters' component={FrequencyOfLetters} />
+      <Route exact path='/GagueCircle1' component={GagueCircle1} />
+      <Route exact path='/LinkedListEditor' component={LinkedListEditor} />
+      <Route exact path='/LineChart' component={LineChart} />
+      <Route exact path='/LineEditor' component={LineEditor} />
+      <Route exact path='/Modules' component={Modules} />
+      <Route exact path='/Pie1' component={Pie1} />
+      <Route exact path='/ScaleEditor' component={ScaleEditor} />
+      <Route exact path='/ShapeEditor' component={ShapeEditor} />
+      <Route exact path='/Playground' component={Playground} />
+      <Route exact path='/StackChart' component={StackChart} />
+      <Route exact path='/Top5States' component={Top5States} />
+      <Route exact path='/TopoLower48' component={TopoLower48} />
+      <Route exact path='/Tux_SVG' component={Tux_SVG} />
+      <Route exact path='/USMap1' component={USMap1} />
+
+    </Switch>
+    
+  </div>
+);
 
 export default App;
