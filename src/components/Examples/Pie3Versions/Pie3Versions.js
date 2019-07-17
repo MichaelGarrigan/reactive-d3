8ab3aeab545b4e755cde4fbaec06d07dc70ef62e
M-garrigan <mgarrigan99@gmail.com>
2019-07-17 16:46:49 -0400

    A component to show three variations of using d3 in react.
    The three variations are Class based components, Hook based and
    directly using the svg components used by d3.

---
+++ src/components/Examples/Pie3Versions/Pie3Versions.js
@@ -0,0 +1,63 @@
+
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import * as d3 from "d3";
+import PieClass from "./PieClass";
+import PieHooks from "./PieHooks";
+import PieSVG from "./PieSVG";
+
+import "./Pie3Versions.css";
+
+const Pie3Versions = props => {
+
+  const generateData = (value, length = 5) =>
+    d3.range(length).map((item, index) => ({
+      date: index,
+      value: value === null || value === undefined ? Math.random() * 100 : value
+    }));
+
+  const [data, setData] = useState(generateData());
+  const changeData = () => {
+    setData(generateData());
+  };
+
+  return (
+    <div className="pie-wrapper">                                                                   
+      <div>
+        <button onClick={changeData}>Transform</button>
+      </div>
+      <div>
+        <span className="label">React Class</span>
+        <PieClass
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={60}
+          outerRadius={100}
+        />
+      </div>
+      <div>
+        <span className="label">Hooks</span>
+        <PieHooks
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={60}
+          outerRadius={100}
+        />
+      </div>
+      <div>
+        <span className="label">SVG Elements</span>
+        <PieSVG
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={60}
+          outerRadius={100}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Pie3Versions;
