26a76918cfe56bf473d74061a86c7c57c840037f
M-garrigan <mgarrigan99@gmail.com>
2019-07-17 17:04:18 -0400

    Renamed file extension to .js

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+const NavBar = () => (
+  <div className="navbar-wrapper">
+    <Link to='/'>
+      <p className="app-title">threact.com</p>
+    </Link>
+
+    <div className="nav-link-group">
+      <Link to="/Examples">
+        <div className="nav-link">Examples</div>
+      </Link>
+      <Link to="/Modules">
+        <div className="nav-link">Modules</div>
+      </Link>
+      <Link to="/EditorShell">
+        <div className="nav-link">Editors</div>
+      </Link>
+      <Link to="/Blog">
+        <div className="nav-link">Blog</div>
+      </Link>
+    </div>
+  </div>
+);
+
+export default NavBar;
