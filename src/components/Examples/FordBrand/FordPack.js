0eaeb53932645d8faae81aba9a3a87f9f09b5d57
M-garrigan <mgarrigan99@gmail.com>
2019-07-24 17:25:02 -0400

    Roughed out the first half of this component.
    This component will house a hierartical bubble chart and a bar chart.
    The bubble chart will show the single bubble as the default and when
    the 'All' button is selected. And will show the specific bubble chart
    when a specific category is selected, as in 'Cars' or 'SUVs'.

--- src/components/Examples/FordBrand/FordPack.js
+++ src/components/Examples/FordBrand/FordPack.js
@@ -1,20 +1,63 @@
-import React from 'react';
+import React, { useState, useCallback, useRef, useEffect, useLayoutEffect } from 'react';
 
-import {pack} from 'd3-hierarchy';
+import { pack, hierarchy, nest } from 'd3-hierarchy';
+import { select, selectAll } from 'd3-selection';
+import { scaleSequential } from 'd3-scale';
+import { interpolateInferno } from 'd3-scale-chromatic';
 
 import './Ford.css';
 
 const FordPack = props => {
+  const { width, height } = props.dimensions;
+  const [svgNode, setSvgNode] = useState(null);
+
+  const color = scaleSequential([8, 0], interpolateInferno);
+
+  const svgRef = useCallback(node => {
+      setSvgNode(node);
+  }, [svgNode]);
+
+  const fordPack = pack()
+    .size([width*.80, width*.80])
+    .padding(3);
+
+  const root = hierarchy(props.data)
+    .sum(d => d[props.year])
+    .sort((a,b) => b[props.year] - a[props.year]);
+    
+  fordPack(root);
+
+  useLayoutEffect( () => {
+    if (svgNode) {
+      select(svgNode)
+        .selectAll("circle")
+        .data(root.descendants())
+        .enter()
+        .append("circle")
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y)
+        .attr('r', d => d.r)
+        .attr('fill', d => color(d.height))
+
+        .append("text").attr("dy", 5)
+        .attr('x', d => d.x - 20)
+        .attr('y', d => d.y)
+        .text( d => d.children === undefined ? d.data.name : '');
+    }
+  }, [svgNode]);
+
+  // console.log(fordPack(root))
 
   return (
     <div className="ford-pack-wrapper">
       <svg 
-          className="svg-ford-pack" 
-          // width={dimensions.width} 
-          // height={height}
-        >
+        className="svg-ford-pack" 
+        ref={svgRef}
+        width={width / 2} 
+        height={width / 2}
+      />
           
-      </svg>
+      <div className="ford-pack-bar-wrapper"></div>
     </div>
   );
 };
