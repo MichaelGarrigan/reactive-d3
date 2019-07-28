1fd20476bf0402e69bcd48c12afeb61165dbcf45
M-garrigan <mgarrigan99@gmail.com>
2019-07-28 15:31:02 -0400

    Made the pack bubble responsive to page width

--- src/components/Examples/FordBrand/FordPack.js
+++ src/components/Examples/FordBrand/FordPack.js
@@ -1,63 +1,64 @@
-import React, { useState, useCallback, useRef, useEffect, useLayoutEffect } from 'react';
+import React from 'react';
 
-import { pack, hierarchy, nest } from 'd3-hierarchy';
-import { select, selectAll } from 'd3-selection';
+import { pack, hierarchy } from 'd3-hierarchy';
 import { scaleSequential } from 'd3-scale';
 import { interpolateInferno } from 'd3-scale-chromatic';
 
+import VerticalBar from './VerticalBar.js';
 import './Ford.css';
 
-const FordPack = props => {
-  const { width, height } = props.dimensions;
-  const [svgNode, setSvgNode] = useState(null);
+// TODO :: Add a toolbar for hover over each circle
+// TODO :: 
 
+const FordPack = props => {
+  const { width } = props.dimensions;
+  
   const color = scaleSequential([8, 0], interpolateInferno);
 
-  const svgRef = useCallback(node => {
-      setSvgNode(node);
-  }, [svgNode]);
-
   const fordPack = pack()
-    .size([width*.80, width*.80])
+    .size([ width / 2, width / 2 ])
     .padding(3);
 
   const root = hierarchy(props.data)
     .sum(d => d[props.year])
     .sort((a,b) => b[props.year] - a[props.year]);
-    
+  
   fordPack(root);
 
-  useLayoutEffect( () => {
-    if (svgNode) {
-      select(svgNode)
-        .selectAll("circle")
-        .data(root.descendants())
-        .enter()
-        .append("circle")
-        .attr('cx', d => d.x)
-        .attr('cy', d => d.y)
-        .attr('r', d => d.r)
-        .attr('fill', d => color(d.height))
-
-        .append("text").attr("dy", 5)
-        .attr('x', d => d.x - 20)
-        .attr('y', d => d.y)
-        .text( d => d.children === undefined ? d.data.name : '');
-    }
-  }, [svgNode]);
-
-  // console.log(fordPack(root))
-
+  // Code snippet to show text over each circle
+  // .append("text").attr("dy", 5)
+  // .attr('x', d => d.x - 20)
+  // .attr('y', d => d.y)
+  // .text( d => d.children === undefined ? d.data.name : '');
+  
   return (
     <div className="ford-pack-wrapper">
       <svg 
         className="svg-ford-pack" 
-        ref={svgRef}
         width={width / 2} 
         height={width / 2}
-      />
+      >
+       {
+         root.descendants().map( node => (
+           <circle
+            key={node.value}
+            cx={node.x}
+            cy={node.y}
+            r={node.r}
+            fill={color(node.height)}
+           />
+          ))
+       }
+      </svg>
           
-      <div className="ford-pack-bar-wrapper"></div>
+      <VerticalBar
+        category={props.category}
+        data={props.data}
+        dimensions={props.dimensions}
+        rankBy={props.rankBy}
+        view={props.view}
+        year={props.year}
+      />
     </div>
   );
 };
