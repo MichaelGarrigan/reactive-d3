3c19f5e0a59039e394cbabb3826502819878f7c1
M-garrigan <mgarrigan99@gmail.com>
2019-08-07 08:49:06 -0400

    An array of strings that are all the available d3 curve types

---
+++ src/components/Examples/LineChart/curveTypes.js
@@ -0,0 +1,21 @@
+
+export default [
+  'curveBasis', 
+  'curveBasisClosed', 
+  'curveBasisOpen', 
+  'curveBundle',
+  'curveCardinal', 
+  'curveCardinalClosed', 
+  'curveCardinalOpen',
+  'curveCatmullRom', 
+  'curveCatmullRomClosed',
+  'curveCatmullRomOpen', 
+  'curveLinear', 
+  'curveLinearClosed',
+  'curveMonotoneX',
+  'curveMonotoneY',
+  'curveNatural',
+  'curveStep',
+  'curveStepAfter', 
+  'curveStepBefore'
+];
