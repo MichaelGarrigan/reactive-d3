b824bd9df25b770e8fe55ada3c31891342724a00
M-garrigan <mgarrigan99@gmail.com>
2019-08-09 14:45:42 -0400

    A starter collection of websites dealing with data visualization

---
+++ src/components/Resources/webResources.js
@@ -0,0 +1,12 @@
+
+const websites = [
+  'storytellingwithdata.com',
+  'public.tableau.com',
+  'datavisualizationsociety.com',
+  'observablehq.com',
+  'bl.ocks.org',
+  'graphviz.org',
+  'flowingdata.com',
+  'alignedleft.com  (Scott Murray)',
+  'victorhou.com  (Minify your SVGs)'
+];
