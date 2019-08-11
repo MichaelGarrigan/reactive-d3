45b2dc44b071ac3aed9cd7d1925d04d5fda2787e
M-garrigan <mgarrigan99@gmail.com>
2019-08-11 10:07:25 -0400

    Destructured props

--- src/components/Examples/FordBrand/FordSalesTotal.js
+++ src/components/Examples/FordBrand/FordSalesTotal.js
@@ -3,21 +3,21 @@ import React from 'react';
 import { lookupMainCategory } from './helperFunctions.js';
 import './Ford.css';
 
-const FordSalesTotal = props => {
-  const mainCategory = lookupMainCategory(props.data.children, props.category);
+const FordSalesTotal = ({category, DATA, year}) => {
+  const mainCategory = lookupMainCategory(DATA.children, category);
 
   return (
     <div className="ford-sales-wrapper">
       {
-        props.category === "All" 
+        category === "All" 
           ? (
             <div className="ford-sales-total">
-              <h3>{`${props.year} Total Sales: ${props.data[props.year + '_rounded']}`}</h3>
+              <h3>{`${year} Total Sales: ${DATA[`${year}_rounded`]}`}</h3>
             </div>
           )
           : (
             <div className="ford-sales-total">
-              <h3>{`${props.year} ${props.category.slice(0, -1)} Sales: ${mainCategory[`${props.year}_rounded`]}`}</h3>
+              <h3>{`${year} ${category.slice(0, -1)} Sales: ${mainCategory[`${year}_rounded`]}`}</h3>
             </div>
           )
       }
