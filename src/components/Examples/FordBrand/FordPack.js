d6d54cd46d067a0dbc2685064ee5ba35e0526464
M-garrigan <mgarrigan99@gmail.com>
2019-08-14 13:11:41 -0400

    Removed VerticalBar component

--- src/components/Examples/FordBrand/FordPack.js
+++ src/components/Examples/FordBrand/FordPack.js
@@ -1,64 +1,75 @@
 import React from 'react';
+import { sortMainCategoriesByYear } from './helperFunctions.js';
 
 import { pack, hierarchy } from 'd3-hierarchy';
 import { scaleSequential } from 'd3-scale';
 import { interpolateInferno } from 'd3-scale-chromatic';
 
-import VerticalBar from './VerticalBar.js';
 import './Ford.css';
 
-// TODO :: Add a toolbar for hover over each circle
-// TODO :: Make the pack graphic for each category
 
 const FordPack = props => {
+  const sortedCategories = sortMainCategoriesByYear(props.DATA.children, props.year);
+
   const { width } = props.dimensions;
+  const svgWidth = Math.round(width * 0.7);
   
   const color = scaleSequential([8, 0], interpolateInferno);
 
-  const fordPack = pack()
-    .size([ width / 2, width / 2 ])
+  const handleFocus = e => {
+    console.log('circle: ', e.target)
+  }
+
+  let fordPack = pack()
+    .size([ svgWidth, svgWidth ])
     .padding(3);
 
-  const root = hierarchy(props.data)
-    .sum(d => d[props.year])
-    .sort((a,b) => b[props.year] - a[props.year]);
-  
-  fordPack(root);
+  let root;
 
-  // Code snippet to show text over each circle
-  // .append("text").attr("dy", 5)
-  // .attr('x', d => d.x - 20)
-  // .attr('y', d => d.y)
-  // .text( d => d.children === undefined ? d.data.name : '');
+  if (props.category === 'All') {
+
+    root = hierarchy(props.DATA)
+      .sum(d => d[props.year])
+      .sort((a,b) => b[props.year] - a[props.year]);
+  
+    fordPack(root);
+  } else {
+    let selectCat;
+    for (let obj of sortedCategories) {
+      if (props.category === obj.name) {
+        selectCat = obj;
+      }
+    }
+
+    root = hierarchy(selectCat)
+      .sum(d => d[props.year])
+      .sort((a,b) => b[props.year] - a[props.year]);
+  
+    fordPack(root);
+  }
   
   return (
     <div className="ford-pack-wrapper">
+    
       <svg 
         className="svg-ford-pack" 
-        width={width / 2} 
-        height={width / 2}
+        width={svgWidth} 
+        height={svgWidth}
       >
-       {
-         root.descendants().map( node => (
-           <circle
+      {
+        root.descendants().map( node => (
+          <circle
             key={node.value}
             cx={node.x}
             cy={node.y}
             r={node.r}
             fill={color(node.height)}
-           />
+            onFocus={e => handleFocus(e)}
+          />
           ))
-       }
+      }
       </svg>
-          
-      <VerticalBar
-        category={props.category}
-        data={props.data}
-        dimensions={props.dimensions}
-        rankBy={props.rankBy}
-        view={props.view}
-        year={props.year}
-      />
+            
     </div>
   );
 };
