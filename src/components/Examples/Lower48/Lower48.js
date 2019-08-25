0926ac3c078de61958f3b738c4a8cdb66e848068
M-garrigan <mgarrigan99@gmail.com>
2019-08-25 14:51:57 -0400

    Remaking a class component into a functional component

--- src/components/Examples/Lower48/Lower48.js
+++ src/components/Examples/Lower48/Lower48.js
@@ -1,69 +1,82 @@
-import React, { Component } from 'react';
+import React, { Component, useCallback, useEffect, useLayoutEffect, useState } from 'react';
 import * as topojson from 'topojson-client';
-import * as d3 from 'd3';
+
+import states from '../../../../node_modules/us-atlas/us/states-10m.json';
+
+import { geoPath } from 'd3-geo';
+import { select } from 'd3-selection';
+import TitleBanner from '../titleBanner/TitleBanner.js';
+
 import './Lower48.css';
 
-export default class TopoLower48 extends Component {
-  state = {
-    lower48data: null
-  }
 
-  componentDidMount() {
-    if (!this.state.lower48data) {
-      d3.json("https://unpkg.com/us-atlas@1/us/10m.json")
-        .then(response => {
-          let us = response;
-          us.objects.lower48 = {
-            type: "GeometryCollection",
-            geometries: us.objects.states.geometries.filter(d => d.id !== "02" && d.id !== "15")
-          };
-          this.setState({lower48data: us});
-        })
-    }
-  }
+export default props => {
+  
+  const [stateName, setStateName] = useState('click a state to see name');
+  
+  // calc actual size of projection
+  let bbox = topojson.bbox(states);
+  let width = bbox[2] + 30;
+  let height = bbox[3] - bbox[1] + 30;
 
-  render() {
-    const path = d3.geoPath();
 
-    if (this.state.lower48data) {
-      return (
-        <svg
-          className='lower48-svg'
-          height='600'
-          width='960'
-        >
-          <path
-            className='lower48-path-states'
-            ref={
-              node => d3.select(node)
-                .datum(
-                  topojson.merge(
-                    this.state.lower48data, 
-                    this.state.lower48data.objects.lower48.geometries
-                  ))
-                .attr('d', path)
-            }
-          />
-          <path
-            fill='none'
-            stroke='black'
-            strokeLinejoin='round'
-            ref={
-              node => d3.select(node)
-                .datum(
-                  topojson.mesh(
-                    this.state.lower48data, 
-                    this.state.lower48data.objects.lower48, 
-                    (a,b) => a !== b)
-                  )
-                .attr('d', path)
-            }
-          />
-        </svg>
-      )
-    } else {
-      return (<div>Not loaded yet ....</div>)
-    }
+  useEffect( () => {
+    return () => props.setRoute([]);
+  }, []);
+
+  
+  const path = geoPath();
+
+  // remove Alaska and Hawaii
+  states.objects.states.geometries = states.objects.states.geometries.filter(d => d.id !== "02" && d.id !== "15");
     
-  }
+
+  return (
+    <div className="lower48-wrapper">
+      <TitleBanner title='US Lower 48 States' />
+      <p className="lower48-p">{stateName}</p>
+      <svg
+        className='lower48-svg'
+        height={height}
+        width={width}
+      >
+      
+        {
+          topojson.feature(states, states.objects.states).features.map( (item, idx) => {
+            // console.log('area: ', path.area(item))
+            // console.log('bounds: ', path.bounds(item))
+            // console.log('centroid: ', path.centroid(item))
+            // console.log('measure: ', path.measure(item))
+            return (
+              <path
+                key={idx}
+                className='lower48-path-states'
+                onClick={ () => setStateName(item.properties.name) }
+                ref={ node => select(node).attr('d', () => path(item)) }
+              />
+            )
+          })
+        }
+          
+        <path
+          fill='none'
+          stroke='black'
+          strokeLinejoin='round'
+          ref={
+            node => select(node)
+              .datum(
+                topojson.mesh(
+                  states, 
+                  states.objects.states, 
+                  // (a,b) => a === b // only outer border
+                  // (a,b) => a !== b // only inner border
+                )
+                )
+              .attr('d', path)
+          }
+        />
+        
+      </svg>
+    </div>
+  )
 }
