c02952741bfdb21f605168b3ebd35cfa9a94cb48
M-garrigan <mgarrigan99@gmail.com>
2019-08-25 14:34:04 -0400

    Placing a style and icon on the Lower48 example

--- src/components/Examples/Examples.js
+++ src/components/Examples/Examples.js
@@ -123,6 +123,98 @@ const Examples = props => {
         </div>
       </Link>
 
+      <Link to={`${props.match.url}/lower48`}>
+        <div className="example-wrapper" onClick={() => setRoute(['lower48', Lower48])}>
+          <p className="example-text">US Lower</p>
+          <p className="example-text">48 States</p>
+          <svg 
+            className="example-thumbnail"
+            viewBox="0 0 51.329166 31.220832" 
+          >
+            <g transform="translate(0 -265.78)">
+              <circle cx="33.943" cy="273.88" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="34.01" cy="275.82" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="32.607" cy="275.82" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="33.275" cy="277.69" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="44.434" cy="278.22" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="47.039" cy="268.6" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="13.363" cy="288.65" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="11.092" cy="288.18" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="9.2208" cy="286.98" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="7.0158" cy="285.98" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="5.479" cy="284.3" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="3.8086" cy="282.83" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="3.2741" cy="280.63" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="6.214" cy="267.13" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="6.0136" cy="268.8" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="2.8731" cy="278.36" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="2.4722" cy="275.82" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="2.8731" cy="273.61" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="3.8086" cy="271.68" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="4.5436" cy="269.81" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="4.6772" cy="267.53" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="36.148" cy="290.45" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="38.286" cy="290.59" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="39.155" cy="292.26" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="40.157" cy="293.99" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="43.899" cy="280.03" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="43.832" cy="281.83" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="43.498" cy="284.1" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="41.761" cy="285.71" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="40.491" cy="287.45" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="40.224" cy="289.78" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="41.226" cy="291.72" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="42.162" cy="293.46" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="41.694" cy="295.33" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="44.434" cy="278.22" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="44.434" cy="278.22" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="44.634" cy="271.74" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="35.948" cy="274.82" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="47.307" cy="275.28" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="45.369" cy="276.35" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="34.21" cy="290.32" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="32.54" cy="291.72" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="30.736" cy="291.86" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="28.731" cy="291.32" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="16.237" cy="288.65" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="17.573" cy="290.45" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="19.043" cy="291.79" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="20.847" cy="291.32" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="21.983" cy="292.99" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="26.727" cy="292.19" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="24.856" cy="293.26" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="22.918" cy="294.8" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="24.656" cy="295.6" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="24.054" cy="269.54" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="21.649" cy="269.47" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="19.577" cy="269.34" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="17.506" cy="269.2" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="15.101" cy="268.8" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="12.829" cy="268.47" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="10.557" cy="268" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="8.419" cy="267.4" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="32.874" cy="273.95" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="33.876" cy="271.81" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="32.072" cy="271.28" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="29.801" cy="271.88" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="30.469" cy="270.47" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="28.13" cy="270.14" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="26.192" cy="269.67" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="35.68" cy="273.15" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="45.904" cy="270.14" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="46.572" cy="273.08" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="47.908" cy="271.81" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="47.975" cy="270.01" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="36.683" cy="277.09" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="38.821" cy="276.42" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="40.09" cy="274.75" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="41.894" cy="274.15" r=".4009" className="example-svg-component-fill"/>
+              <circle cx="42.763" cy="272.14" r=".4009" className="example-svg-component-fill"/>
+            </g>
+          </svg>
+        </div>
+      </Link>
+
       <Link to={`${props.match.url}/treemap`}>
         <div className="d3-example" onClick={() => setRoute(['treemap', Treemap])}>
           <span className="d3-example-text">Treemap</span>
@@ -158,13 +250,6 @@ const Examples = props => {
       <Link to={`${props.match.url}/area`}>
         <div className="d3-example area" onClick={() => setRoute(['area', Area])}>
           <span className="d3-example-text">Area Chart</span>
-        </div>
-      </Link>
-
-      <Link to={`${props.match.url}/lower48`}>
-        <div className="d3-example topolower48" onClick={() => setRoute(['lower48', Lower48])}>
-          <span className="d3-example-text">TopoJSON</span>
-          <span className="d3-example-text">Lower 48 States</span>
         </div>
       </Link>
 
