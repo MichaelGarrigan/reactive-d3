8f318a31f9c7eed82e36035590828fe2353e3706
M-garrigan <mgarrigan99@gmail.com>
2019-08-27 13:06:16 -0400

    Data for annual rainfall for selected cities

---
+++ src/components/Examples/Area/AreaData.js
@@ -0,0 +1,47 @@
+
+export default {
+  'Alabama': {
+    city: 'Birmingham',
+    state: 'Alabama',
+    data: [
+      { year: 2015, rain: 52.60 },
+      { year: 2016, rain: 59.83 },
+      { year: 2017, rain: 43.77 },
+      { year: 2018, rain: 63.69 },
+      { year: 2019, rain: 65.78 }
+    ]
+  },
+  'Atlanta': {
+    city: 'Atlanta',
+    state: 'Georgia',
+    data: [
+      { year: 2015, rain: 48.65 },
+      { year: 2016, rain: 69.18 },
+      { year: 2017, rain: 41.74 },
+      { year: 2018, rain: 47.58 },
+      { year: 2019, rain: 73.03 }
+    ]
+  },
+  'NYC': {
+    city: 'NYC',
+    state: 'New York',
+    data: [
+      { year: 2015, rain: 56.23 },
+      { year: 2016, rain: 40.17 },
+      { year: 2017, rain: 42.63 },
+      { year: 2018, rain: 42.42 },
+      { year: 2019, rain: 66.96 }
+    ]
+  },
+  'Scranton': {
+    city: 'Scranton',
+    state: 'Pennsylvania',
+    data: [
+      { year: 2015, rain: 30.68 },
+      { year: 2016, rain: 31.13 },
+      { year: 2017, rain: 32.78 },
+      { year: 2018, rain: 36.97 },
+      { year: 2019, rain: 62.55 }
+    ]
+  }
+};
