5e81fb8b9439cd1c1e9e104982f057b6f5bdae40
M-garrigan <mgarrigan99@gmail.com>
2019-08-28 14:24:28 -0400

    Starting to add categories to Resources component

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default props => {
   return (
-    <div className="resources-wrapper"></div>
+    <div className="resources-wrapper">
+      <h2>Web Pages</h2>
+
+      <h2>Data Sources</h2>
+
+      <h2>Color / Graphic</h2>
+
+      <h2>Web Pages</h2>
+
+      <h2>People</h2>
+    </div>
   )
 }
