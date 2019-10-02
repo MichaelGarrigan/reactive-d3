b9ff6652dcc0fdb05e4be75c775e6fa1f64e3394
M-garrigan <mgarrigan99@gmail.com>
2019-10-02 18:31:28 -0400

    Module data in json format:

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -6,6 +6,7 @@ export default props => {
       <h2>Web Pages</h2>
 
       <h2>Data Sources</h2>
+      <p>naturalearthdata.com</p>
 
       <h2>Color / Graphic</h2>
 
@@ -15,6 +16,22 @@ export default props => {
       <p>Two-n.com</p>
 
       <h2>People</h2>
+
+      <h2>Other</h2>
+      <h4>Companies that sponsored OpenVis 2017 Conf</h4>
+      <p>bocoup</p>
+      <p>qlik playground</p>
+      <p>datadog</p>
+      <p>actblue</p>
+      <p>edward tufte graphics press</p>
+      <p>maps 4 news</p>
+      <p>fjord</p>
+      <p>info viz</p>
+      <p>mlab</p>
+      <p>policyviz</p>
+
+      <h2>SVG & Canvas</h2>
+      <p>https://stackoverflow.com/questions/50141324/difference-between-svg-and-canvas-in-d3-js</p>
     </div>
   )
 }
