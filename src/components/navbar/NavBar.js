09e27b859854a10fcfebadc9c4dbb1ac7bc85ea7
M-garrigan <mgarrigan99@gmail.com>
2019-10-03 16:02:30 -0400

    Changing the app title

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,20 +3,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
-const NavBar = () => (
+const NavBar = ({dimensions}) => (
   <div className="navbar-wrapper">
-    <Link to='/'>
-      <p className="app-title">threact.com</p>
+    <Link to='/' className="nav-link-title">
+      <div 
+        className="nav-title-grid"
+        style={{
+          height: Math.round(dimensions.width * 0.1),
+          width: Math.round(dimensions.width * 0.25)
+        }}
+      >
+        <div className="nav-bar-1"></div>
+        <div className="nav-bar-2"></div>
+        <div className="nav-bar-3"></div>
+        <div className="nav-bar-4"></div>
+        <div className="nav-bar-5"></div>
+        <div className="nav-bar-6"></div>
+        
+        <div 
+          className="nav-title-wrapper"
+          style={{ fontSize: Math.round(dimensions.width * 0.05) }}
+        >
+          <p className="nav-title">reactive</p>
+          <p className="nav-title">d3</p>
+        </div>
+      </div>
     </Link>
 
     <div className="nav-link-group">
-      <Link to="/Examples">
+      <Link to="/Examples" className="nav-examples-link">
         <div className="nav-link">Examples</div>
       </Link>
-      <Link to="/Modules">
+      <Link to="/Modules" className="nav-modules-link">
         <div className="nav-link">Modules</div>
       </Link>
-      <Link to="/Resources">
+      <Link to="/Resources" className="nav-resources-link">
         <div className="nav-link">Resources</div>
       </Link>
     </div>
