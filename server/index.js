b34c8d46fb31e466aac4cdaf960631af3a732f11
M-garrigan <mgarrigan99@gmail.com>
2019-10-10 17:44:00 -0400

    Cleaning up formatting

--- server/index.js
+++ server/index.js
@@ -2,11 +2,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
+
+const file = json => {
+  fs.writeFile("zoom.json", json, (err) => {
+    if (err) console.log(err);
+    console.log("Successfully Written to File.");
+  });
+};
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/../dist')));
 app.use(bodyParser.json());
+
+app.post('/d3', (req, res) => {
+  // file(req.body.name);
+});
 
 app.get('/*', (req, res) => {
   res.sendFile(
--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,24 @@ import { Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 
 import Examples from './Examples/Examples.js';
-import Home from './home/Home.jsx';
+import Home from './home/Home.js';
 import Modules from './modules/Modules.js';
 import NavBar from './navbar/NavBar.js';
 import Resources from './Resources/Resources.js';
 
+import useElementSize from '../useElementSize.js';
 
-const App = () => (
-  <div className="global-container">
-    <NavBar />
+export default () => {
+  const [sizeRef, dimensions] = useElementSize();
+  return (
+    <div className="global-container" ref={sizeRef}>
+      <NavBar dimensions={dimensions} />
 
-    <Route exact path='/' component={Home} />
-    <Route path='/Examples' component={Examples} />
-    <Route exact path='/Modules' component={Modules} />
-    <Route exact path='/Resources' component={Resources} />
+      <Route exact path='/' component={() => <Home dimensions={dimensions} />} />
+      <Route path='/Examples' component={Examples} />
+      <Route exact path='/Modules' component={() => <Modules dimensions={dimensions} />} />
+      <Route path='/Resources' component={() => <Resources dimensions={dimensions} />} />
 
-  </div>
-);
-
-export default App;
+    </div>
+  );
+};
