8a2ccd9cf2828fe18331ee5bf2cf21c1a5db58a3
M-garrigan <mgarrigan99@gmail.com>
2019-10-10 17:13:16 -0400

    Make the links have a bottom border when hovered or clicked

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,47 +1,61 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
-const NavBar = ({dimensions}) => (
-  <div className="navbar-wrapper">
-    <Link to='/' className="nav-link-title">
-      <div 
-        className="nav-title-grid"
-        style={{
-          height: Math.round(dimensions.width * 0.1),
-          width: Math.round(dimensions.width * 0.25)
-        }}
+export default ({dimensions}) => {
+  const [category, setCategory] = useState('');
+
+  return (
+    <div className="navbar-wrapper">
+      <Link to='/' 
+        className="nav-link-title"
+        onClick={() => setCategory('')}
       >
-        <div className="nav-bar-1"></div>
-        <div className="nav-bar-2"></div>
-        <div className="nav-bar-3"></div>
-        <div className="nav-bar-4"></div>
-        <div className="nav-bar-5"></div>
-        <div className="nav-bar-6"></div>
-        
         <div 
-          className="nav-title-wrapper"
-          style={{ fontSize: Math.round(dimensions.width * 0.05) }}
+          className="nav-title-grid"
+          style={{
+            height: Math.round(dimensions.width * 0.1),
+            width: Math.round(dimensions.width * 0.25)
+          }}
         >
-          <p className="nav-title">reactive</p>
-          <p className="nav-title">d3</p>
+          <div className="nav-bar-1"></div>
+          <div className="nav-bar-2"></div>
+          <div className="nav-bar-3"></div>
+          <div className="nav-bar-4"></div>
+          <div className="nav-bar-5"></div>
+          <div className="nav-bar-6"></div>
+        
+          <div 
+            className="nav-title-wrapper"
+            style={{ fontSize: Math.round(dimensions.width * 0.05) }}
+          >
+            <p className="nav-title-react">reactive</p>
+            <p className="nav-title-d3">d3</p>
+          </div>
         </div>
+      </Link>
+
+      <div className="nav-link-group">
+        <Link to="/Examples" 
+          className={category === "examples" ? "nav-link-active" : "nav-examples-link"}
+          onClick={() => setCategory('examples')}
+        >
+          <div className="nav-link">Examples</div>
+        </Link>
+        <Link to="/Modules" 
+          className={category === "modules" ? "nav-link-active" : "nav-modules-link"}
+          onClick={() => setCategory('modules')}
+        >
+          <div className="nav-link">Modules</div>
+        </Link>
+        <Link to="/Resources" 
+          className={category === "resources" ? "nav-link-active" : "nav-resources-link"}
+          onClick={() => setCategory('resources')}
+        >
+          <div className="nav-link">Resources</div>
+        </Link>
       </div>
-    </Link>
-
-    <div className="nav-link-group">
-      <Link to="/Examples" className="nav-examples-link">
-        <div className="nav-link">Examples</div>
-      </Link>
-      <Link to="/Modules" className="nav-modules-link">
-        <div className="nav-link">Modules</div>
-      </Link>
-      <Link to="/Resources" className="nav-resources-link">
-        <div className="nav-link">Resources</div>
-      </Link>
     </div>
-  </div>
-);
-
-export default NavBar;
+  );
+};
