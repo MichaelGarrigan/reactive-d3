1c230ce8153af76b68c069dba19c166c99be65f6
M-garrigan <mgarrigan99@gmail.com>
2019-11-18 04:03:54 -0500

    Simplifing the banner and moving to ExampleHelpers dir

--- src/components/ExampleHelpers/Banner/Banner.js
+++ src/components/ExampleHelpers/Banner/Banner.js
@@ -2,5 +2,9 @@ import React from 'react';
 import './Banner.css';
 
 export default ({title}) => (
-  <div className="example-banner">{title}</div>
+  <div className="example-banner">
+    {title}
+    <div className="example-banner-left"></div>
+    <div className="example-banner-right"></div>
+  </div>
 );
