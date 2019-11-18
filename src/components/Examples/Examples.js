5e8b1274cae7d6201188bb328e74b4582280b356
M-garrigan <mgarrigan99@gmail.com>
2019-11-18 04:09:37 -0500

    Splitting up the structure of the examples component

--- src/components/Examples/Examples.js
+++ src/components/Examples/Examples.js
@@ -15,15 +15,29 @@ import Lower48 from './Lower48/Lower48.js';
 import Pie3Versions from './Pie3Versions/Pie3Versions.js';
 import Treemap from './Treemap/Treemap.js';
 import Tux from './Tux/Tux.js';
-import WorldClocks from './Clocks/WorldClocks.js';
 
+import { Clocks, ClocksCode, ClocksSummary } from './Clocks/index.js';
+import ClocksSVG from '../ExampleHelpers/ThumbnailSvgs/ClocksSVG.js';
+
+import ExampleTemplate from '../ExampleHelpers/ExampleTemplate.js';
 
 export default props => {
   let { path } = useRouteMatch();
   
   return (
     <Switch>
-      <Route path={`${path}/worldClocks`} component={() => <WorldClocks dimensions={props.dimensions} />} />
+      <Route 
+        path={`${path}/clocks`} 
+        component={() => 
+          <ExampleTemplate 
+            dimensions={props.dimensions} 
+            title='Classic World Clocks'
+            demo={Clocks}
+            code={ClocksCode}
+            summary={ClocksSummary}
+          />
+        } 
+      />
       <Route path={`${path}/ford`} component={() => <Ford dimensions={props.dimensions} />} />
       <Route path={`${path}/barcharts`} component={() => <BarCharts dimensions={props.dimensions} />} />
       <Route path={`${path}/linechart`} component={() => <LineChart dimensions={props.dimensions} />} />
@@ -42,29 +56,11 @@ export default props => {
   
         <div className="examples-wrapper">
         
-        <Link to={`${path}/worldClocks`}>
+        <Link to={`${path}/clocks`}>
           <div className="example-wrapper" >
             <p className="example-text">World</p>
             <p className="example-text">Clocks</p>
-            <svg 
-              className="example-thumbnail"
-              viewBox="0 0 51.329165 31.220834" 
-            >
-              <g transform="translate(0 -265.78)">
-                <g transform="translate(.20045 .60136)">
-                  <path 
-                    className="example-svg-component-fill"
-                    transform="matrix(.26458 0 0 .26458 0 265.78)" 
-                    d="m29.162 10.922c8.5798 11.273 12.462 22.611 13.734 26.936a8.0812 7.7024 50.307 0 0 0.42188 1.4746c0.05065 0.19805 0.11133 0.44336 0.11133 0.44336l0.05078-0.04102a8.0812 7.7024 50.307 0 0 1.209 1.9512 8.0812 7.7024 50.307 0 0 8.4648 2.6895l46.516 56.041 4.4687-3.7109-46.514-56.039a8.0812 7.7024 50.307 0 0 -1.084 -8.8203 8.0812 7.7024 50.307 0 0 -4.1426 -2.6973c-4.4426-2.3447-14.325-8.228-23.236-18.227zm68.318 79.551a2.5254 2.5254 0 0 1 2.5254 2.5254 2.5254 2.5254 0 0 1 -2.5254 2.5254 2.5254 2.5254 0 0 1 -2.5254 -2.5254 2.5254 2.5254 0 0 1 2.5254 -2.5254z" 
-                  />
-                  <path 
-                    className="example-svg-component-fill"
-                    transform="matrix(.26458 0 0 .26458 0 265.78)" 
-                    d="m161.8 31.949c-10.713 9.2696-21.785 13.853-26.021 15.395a7.7024 8.0812 46.713 0 0 -1.4473 0.51367c-0.19457 0.06312-0.4336 0.13867-0.4336 0.13867l0.043 0.04687a7.7024 8.0812 46.713 0 0 -1.8711 1.3301 7.7024 8.0812 46.713 0 0 -2.5234 7.2324l-39.906 37.588 5.7148 6.0664 39.914-37.596a7.7024 8.0812 46.713 0 0 7.3633 -2.0781 7.7024 8.0812 46.713 0 0 2.4336 -4.3047c2.0616-4.5809 7.3141-14.811 16.734-24.332zm-64.578 58.271a2.5254 2.5254 0 0 1 2.5254 2.5254 2.5254 2.5254 0 0 1 -2.5254 2.5254 2.5254 2.5254 0 0 1 -2.5254 -2.5254 2.5254 2.5254 0 0 1 2.5254 -2.5254z" 
-                  />
-                </g>
-              </g>
-            </svg>
+            { <ClocksSVG /> }
           </div>
         </Link>
 
