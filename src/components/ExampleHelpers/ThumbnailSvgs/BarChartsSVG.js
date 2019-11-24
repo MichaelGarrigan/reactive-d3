ff9906f62f6d04d25f1d22a902c98d0516094cc2
M-garrigan <mgarrigan99@gmail.com>
2019-11-24 17:25:41 -0500

    Moving the svg to its own component

---
+++ src/components/ExampleHelpers/ThumbnailSvgs/BarChartsSVG.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+export default () => (
+  <svg 
+    className="example-thumbnail"
+    viewBox="0 0 51.329165 31.220834" 
+  >
+    <g transform="translate(0 -265.78)">
+      <path 
+        className="example-svg-component-stroke"
+        d="m6.6632 289.14 7.843-0.0472-0.0945-15.119 7.9375 1e-5 1e-6 15.119 8.1737-1e-5 -0.04725-11.103 7.7485-0.0473v11.198l6.2839 1e-5"
+        fill="none" 
+        strokeWidth="1.9844"
+      />
+    </g>
+  </svg>
+);
