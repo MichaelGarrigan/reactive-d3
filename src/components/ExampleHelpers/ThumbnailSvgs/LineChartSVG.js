b0566c39fd8961d39ab1b5b734b8ea0f54889a1e
M-garrigan <mgarrigan99@gmail.com>
2019-11-25 19:25:08 -0500

    Moving a svg to its own component

---
+++ src/components/ExampleHelpers/ThumbnailSvgs/LineChartSVG.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => (
+  <svg 
+    className="example-thumbnail"
+    viewBox="0 0 51.329165 31.220834" 
+  >
+    <g transform="translate(0 -265.78)">
+      <path 
+        className="example-svg-component-stroke"
+        d="m9.3544 288.58 9.0872-12.027 6.949 14.433 12.963-19.11 3.4745 8.2854" 
+        fill="none" 
+        strokeWidth="1.9844"
+      />
+    </g>
+  </svg>
+);
