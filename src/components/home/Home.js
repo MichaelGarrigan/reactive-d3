6b979d8734377b456f58cdb83e5f49dfcfd5354f
M-garrigan <mgarrigan99@gmail.com>
2019-11-26 16:38:45 -0500

    Adding background to the landing page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,25 @@
 
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+
+import { range } from 'd3-array';
+import { randomUniform } from 'd3-random';
+import { scaleLinear } from 'd3-scale';
+import { select } from 'd3-selection';
+import  { curveMonotoneX, curveMonotoneY, curveStep, line } from 'd3-shape';
 import './Home.css';
+
+const randomizeData = () => range(60).map(d => ({ "y": randomUniform(1)() }));
+const randomizeData1 = () => range(30).map(d => ({ "y": randomUniform(1)() }));
 
 
 export default ({dimensions}) => {
+
+  const svgWidth = dimensions.width;
+  const svgHeight = Math.round(svgWidth * 0.7);
+  
+  const [dataRandom, setDataRandom] = useState(randomizeData());
+  const [dataRandom1, setDataRandom1] = useState(randomizeData1());
   
   const [verbs, setVerbs] = useState(
     ["Exploring", "Hacking", "Building"]
@@ -40,11 +55,52 @@ export default ({dimensions}) => {
     return () => clearInterval(interval); 
   }, [content, index, word, pause]);
 
+  const xScale = scaleLinear()
+    .domain([0, 59])
+    .range([-25, svgWidth]); 
 
+  const yScale = scaleLinear()
+    .domain([0, 1]) 
+    .range([svgHeight, 0]);
 
+  const lineShape = line()
+    .x( (d, i) => xScale(i+1) ) 
+    .y( d => yScale(d.y) ) 
+    .curve(curveStep);
+
+  const xScale1 = scaleLinear()
+    .domain([0, 29])
+    .range([-25, svgWidth]); 
+
+  const yScale1 = scaleLinear()
+    .domain([1, 0]) 
+    .range([svgHeight - 25, 0]);
+
+  const lineShape1 = line()
+    .x( (d, i) => xScale1(i+1) ) 
+    .y( d => yScale1(d.y) ) 
+    .curve(curveStep);
+
+  
   return (
     <div className="home-wrapper">
       
+      <svg 
+        className="home-svg"
+        height={svgHeight} 
+        width={svgWidth}
+      >
+        <path 
+          className="home-curve"
+          ref={node => select(node).datum(dataRandom).attr("d", lineShape)}
+        />
+         <path 
+          className="home-curve1"
+          ref={node => select(node).datum(dataRandom1).attr("d", lineShape1)}
+        />
+  
+      </svg>
+
       <div className="home-box-dynamic">
         <span className="home-text-dynamic">{content}</span>
       </div>
