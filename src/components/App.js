dc0c277d460f7c69fee051176455d84445d93bc8
M-garrigan <mgarrigan99@gmail.com>
2019-12-01 03:28:48 -0500

    Adding code splitting along routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 
-import Examples from './Examples/Examples.js';
 import Home from './home/Home.js';
-import Modules from './modules/Modules.js';
 import NavBar from './navbar/NavBar.js';
-import Resources from './Resources/Resources.js';
+const LazyExamples = lazy( () => import('./Examples/Examples.js'));
+const LazyModules = lazy( () => import('./modules/Modules.js'));
+const LazyResources = lazy( () => import('./Resources/Resources.js'));
 
 import useElementSize from '../useElementSize.js';
 
@@ -18,10 +18,32 @@ export default () => {
       <NavBar dimensions={dimensions} />
 
       <Route exact path='/' component={() => <Home dimensions={dimensions} />} />
-      <Route path='/Examples' component={() => <Examples dimensions={dimensions} />} />
-      <Route exact path='/Modules' component={() => <Modules dimensions={dimensions} />} />
-      <Route path='/Resources' component={() => <Resources dimensions={dimensions} />} />
-
+      
+      <Route 
+        path='/Examples' 
+        component={ () => (
+          <Suspense fallback={<div>Loading...</div>} >
+            <LazyExamples dimensions={dimensions} />
+          </Suspense>
+        )} 
+      />
+      
+      <Route 
+        path='/Modules' 
+        component={ () => (
+          <Suspense fallback={<div>Loading...</div>} >
+            <LazyModules dimensions={dimensions} />
+          </Suspense>
+        )} 
+      />
+      <Route 
+        path='/Resources' 
+        component={ () => (
+          <Suspense fallback={<div>Loading...</div>} >
+            <LazyResources dimensions={dimensions} />
+          </Suspense>
+        )} 
+      />
     </div>
   );
 };
