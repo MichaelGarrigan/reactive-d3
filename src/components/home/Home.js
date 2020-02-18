2b78c8ae890f929e919edb333c4821fd61625821
M-garrigan <mgarrigan99@gmail.com>
2020-02-17 19:20:42 -0500

    Changing typed style to a fading style

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,141 +5,119 @@ import { range } from 'd3-array';
 import { randomUniform } from 'd3-random';
 import { scaleLinear } from 'd3-scale';
 import { select } from 'd3-selection';
-import  { curveStep, line } from 'd3-shape';
+import { curveStep, line } from 'd3-shape';
 import './Home.css';
 
 
-export default ({dimensions}) => {
-  
+export default ({ dimensions }) => {
+
   const [width, setWidth] = useState(dimensions.width);
   const [height, setHeight] = useState(dimensions.height);
-  
+
   const [lineDarkData, setLineDarkData] = useState(range(8).map(d => ({ "y": randomUniform(1)() })));
   const [lineMedData, setLineMedData] = useState(range(15).map(d => ({ "y": randomUniform(1)() })));
   const [lineLightData, setLineLightData] = useState(range(30).map(d => ({ "y": randomUniform(1)() })));
   const [yellowRect, setYellowRect] = useState('');
-  
-  const [verbs, setVerbs] = useState(
-    ["Exploring", "Hacking", "Building"]
-  );
-  const [content, setContent] = useState('');
+
+  const [verbs] = useState(["Exploring", "Hacking", "Building"]);
   const [index, setIndex] = useState(0);
-  const [word, setWord] = useState(0);
-  const [fadeWordNow, setFadeWordNow] = useState(false);
-  const [pause, setPause] = useState(0);
-  
-  useEffect( () => {
 
-    let build = () => {
-      
-      if (index === verbs[word % 3].length && pause < 6) {
-        setPause(pause + 1);
-      }
-      else if (index < verbs[word % 3].length) {
-        setContent(content + verbs[word % 3][index]);
-        setIndex(index + 1);
-      } 
-      else {
-        setPause(0)
-        setContent('');
-        setIndex(0);
-        setWord(word + 1);
-      }
-    };
-    
-    let interval = setInterval(build, 400);
-    
-    
-    return () => clearInterval(interval); 
-  }, [content, index, word, pause]);
+  useEffect(() => {
+    console.log('cycle: ', index, verbs[index % 3])
+    let cycle = () => { setIndex(index + 1); };
+
+    let interval = setInterval(cycle, 4000);
+
+    return () => clearInterval(interval);
+  }, [index]);
 
 
-  useEffect( () => {
+  useEffect(() => {
     setYellowRect(
       range(25).map(d => (
-        { 
-          x: randomUniform()(), 
+        {
+          x: randomUniform()(),
           y: randomUniform()(),
           height: randomUniform(height * 0.03, height * 0.1)(),
           width: randomUniform(width * 0.01, width * 0.03)()
         }
       ))
     )
-    
+
   }, []);
 
   // dark line shape
   const darkX = scaleLinear()
     .domain([0, 7])
-    .range([0, width]); 
+    .range([0, width]);
 
   const darkY = scaleLinear()
-    .domain([0, 1]) 
+    .domain([0, 1])
     .range([height, 0]);
 
   const darkLine = line()
-    .x( (d, i) => darkX(i) ) 
-    .y( d => darkY(d.y) ) 
+    .x((d, i) => darkX(i))
+    .y(d => darkY(d.y))
     .curve(curveStep);
 
   // med line shape
   const medX = scaleLinear()
     .domain([0, 14])
-    .range([0, width]); 
+    .range([0, width]);
 
   const medY = scaleLinear()
-    .domain([1, 0]) 
+    .domain([1, 0])
     .range([height, 0]);
 
   const medLine = line()
-    .x( (d, i) => medX(i) ) 
-    .y( d => medY(d.y) ) 
+    .x((d, i) => medX(i))
+    .y(d => medY(d.y))
     .curve(curveStep);
 
   // light line shape
   const lightX = scaleLinear()
     .domain([0, 29])
-    .range([0, width]); 
+    .range([0, width]);
 
   const lightY = scaleLinear()
-    .domain([1, 0]) 
-    .range([height , 0]);
+    .domain([1, 0])
+    .range([height, 0]);
 
   const lightLine = line()
-    .x( (d, i) => lightX(i) ) 
-    .y( d => lightY(d.y) ) 
+    .x((d, i) => lightX(i))
+    .y(d => lightY(d.y))
     .curve(curveStep);
 
   const windowScaleX = scaleLinear()
     .domain([0, 1])
-    .range([0, width]); 
+    .range([0, width]);
 
   const windowScaleY = scaleLinear()
-    .domain([0, 1]) 
+    .domain([0, 1])
     .range([height, 0]);
 
-  
+
   return (
     <div className="home-wrapper">
-      
-      <svg 
+
+      <svg
         className="home-svg"
-        height={height} 
+        height={height}
         width={width}
       >
-        <path 
+        <path
           className="home-darkLine"
           ref={node => select(node).datum(lineDarkData).attr("d", darkLine)}
         />
-         <path 
+        <path
           className="home-medLine"
           ref={node => select(node).datum(lineMedData).attr("d", medLine)}
         />
         {
           yellowRect
-          ? 
-            yellowRect.map( (window, idx) => (
-              <rect 
+            ?
+            yellowRect.map((window, idx) => (
+              <rect
                 key={idx}
                 fill="#FEFF3477"
                 x={windowScaleX(window.x)}
@@ -148,26 +126,26 @@ export default ({dimensions}) => {
                 width={window.width}
               />
             ))
-          : ''
+            : ''
         }
 
-        <path 
+        <path
           className="home-lightLine"
           ref={node => select(node).datum(lineLightData).attr("d", lightLine)}
         />
-      
+
       </svg>
 
       <div className="home-box-text-wrapper">
         <div className="home-box-dynamic">
-          <span className="home-text-dynamic">{content}</span>
+          <span className="home-text-dynamic">{verbs[index % 3]}</span>
         </div>
 
         <div className="home-box-static">
           <p className="home-text-static">d3 and React</p>
         </div>
       </div>
-    
+
     </div>
   );
 };
